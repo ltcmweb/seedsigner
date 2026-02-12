63d47b7d2e558018f3cf585018ea58b15653021e
Hector Chu <hectorchu@gmail.com>
2026-02-12 21:14:24 +0000

    Increase QR density

--- src/seedsigner/models/settings_definition.py
+++ src/seedsigner/models/settings_definition.py
@@ -595,7 +595,7 @@ class SettingsDefinition:
                       type=SettingsConstants.TYPE__SELECT_1,
                       visibility=SettingsConstants.VISIBILITY__ADVANCED,
                       selection_options=SettingsConstants.ALL_DENSITIES,
-                      default_value=SettingsConstants.DENSITY__MEDIUM),
+                      default_value=SettingsConstants.DENSITY__HIGH),
 
         SettingsEntry(category=SettingsConstants.CATEGORY__FEATURES,
                       attr_name=SettingsConstants.SETTING__SIG_TYPES,
