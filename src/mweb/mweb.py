7de2ec7ca1f771014813561cf68fc25d0d1a438b
Hector Chu <hectorchu@gmail.com>
2026-02-13 23:10:03 +0000

    Reduce unnecessary dead code in mweb binary

--- src/mweb/mweb.py
+++ src/mweb/mweb.py
@@ -5,16 +5,9 @@ import os
 
 lib = cdll.LoadLibrary(os.path.join(os.path.dirname(__file__), 'mweb'))
 
-class string(Structure):
-    _fields_ = [('p', c_char_p), ('n', c_int)]
-    def __init__(self, s):
-        self.b = s.encode() if isinstance(s, str) else s
-        self.p = c_char_p(self.b)
-        self.n = len(self.b)
-
 def do_req(f, req):
     f.restype = c_void_p
-    p = f(string(json.dumps(req)))
+    p = f(json.dumps(req).encode())
     s = string_at(p).decode()
     try:
         return json.loads(s)
@@ -28,27 +21,27 @@ def b64(b): return b64encode(b).decode()
 
 def addresses(key, i, j):
     return do_req(lib.Addresses, {
-        "scanSecret": b64(key.child(0x80000000).key.secret),
-        "spendPubkey": b64(key.child(0x80000001).key.sec()),
-        "fromIndex": i,
-        "toIndex": j,
-    })["address"]
+        "ScanSecret": b64(key.child(0x80000000).key.secret),
+        "SpendPubkey": b64(key.child(0x80000001).key.sec()),
+        "FromIndex": i,
+        "ToIndex": j,
+    })["Address"]
 
 def psbt_get_recipients(psbtB64):
     return do_req(lib.PsbtGetRecipients, {
-        "psbtB64": psbtB64,
+        "PsbtB64": psbtB64,
     })
 
 def psbt_sign(psbtB64, key):
     return do_req(lib.PsbtSign, {
-        "psbtB64": psbtB64,
-        "scanSecret": b64(key.child(0x80000000).key.secret),
-        "spendSecret": b64(key.child(0x80000001).key.secret),
-    })["psbtB64"]
+        "PsbtB64": psbtB64,
+        "ScanSecret": b64(key.child(0x80000000).key.secret),
+        "SpendSecret": b64(key.child(0x80000001).key.secret),
+    })["PsbtB64"]
 
 def psbt_sign_non_mweb(psbtB64, key, index):
     return do_req(lib.PsbtSignNonMweb, {
-        "psbtB64": psbtB64,
-        "privKey": b64(key),
-        "index": index,
-    })["psbtB64"]
+        "PsbtB64": psbtB64,
+        "PrivKey": b64(key),
+        "Index": index,
+    })["PsbtB64"]
