a1f41ab5ab93ea355910ca56b7afbb84a89aacb9
Hector Chu <hectorchu@gmail.com>
2026-02-20 08:40:26 +0000

    Refactor

--- src/mweb/mweb.go
+++ src/mweb/mweb.go
@@ -13,97 +13,63 @@ import (
 	"github.com/ltcmweb/mwebd/sign"
 )
 
-var cp = chaincfg.MainNetParams
-
-//export Addresses
-func Addresses(s *C.char) *C.char {
-	var req sign.AddressesRequest
+func doReq[Req, Resp any](s *C.char, f func(*Req) (Resp, error)) *C.char {
+	var req Req
 	if err := json.Unmarshal([]byte(C.GoString(s)), &req); err != nil {
 		return C.CString(err.Error())
 	}
-	resp := sign.Addresses(&req, &cp)
-	b, _ := json.Marshal(&resp)
+	resp, err := f(&req)
+	if err != nil {
+		return C.CString(err.Error())
+	}
+	b, _ := json.Marshal(resp)
 	return C.CString(string(b))
+}
+
+//export Addresses
+func Addresses(s *C.char) *C.char {
+	return doReq(s, func(req *sign.AddressesRequest) (sign.AddressesResponse, error) {
+		return sign.Addresses(req, &chaincfg.MainNetParams), nil
+	})
 }
 
 //export AddressesPubKeyHash
 func AddressesPubKeyHash(s *C.char) *C.char {
-	var req sign.AddressesPubKeyHashRequest
-	if err := json.Unmarshal([]byte(C.GoString(s)), &req); err != nil {
-		return C.CString(err.Error())
-	}
-	resp, err := sign.AddressesPubKeyHash(&req, &cp)
-	if err != nil {
-		return C.CString(err.Error())
-	}
-	b, _ := json.Marshal(&resp)
-	return C.CString(string(b))
+	return doReq(s, func(req *sign.AddressesPubKeyHashRequest) (sign.AddressesResponse, error) {
+		return sign.AddressesPubKeyHash(req, &chaincfg.MainNetParams)
+	})
 }
 
 //export PsbtGetRecipients
 func PsbtGetRecipients(s *C.char) *C.char {
-	var req sign.Psbt
-	if err := json.Unmarshal([]byte(C.GoString(s)), &req); err != nil {
-		return C.CString(err.Error())
-	}
-	resp, err := sign.PsbtGetRecipients(&req, &cp)
-	if err != nil {
-		return C.CString(err.Error())
-	}
-	b, _ := json.Marshal(&resp)
-	return C.CString(string(b))
+	return doReq(s, func(req *sign.Psbt) (sign.PsbtGetRecipientsResponse, error) {
+		return sign.PsbtGetRecipients(req, &chaincfg.MainNetParams)
+	})
 }
 
 //export PsbtSign
 func PsbtSign(s *C.char) *C.char {
-	var req sign.PsbtSignRequest
-	if err := json.Unmarshal([]byte(C.GoString(s)), &req); err != nil {
-		return C.CString(err.Error())
-	}
-	resp, err := sign.PsbtSign(&req)
-	if err != nil {
-		return C.CString(err.Error())
-	}
-	b, _ := json.Marshal(&resp)
-	return C.CString(string(b))
+	return doReq(s, sign.PsbtSign)
 }
 
 //export PsbtSignPubKeyHash
 func PsbtSignPubKeyHash(s *C.char) *C.char {
-	var req sign.PsbtSignPubKeyHashRequest
-	if err := json.Unmarshal([]byte(C.GoString(s)), &req); err != nil {
-		return C.CString(err.Error())
-	}
-	resp, err := sign.PsbtSignPubKeyHash(&req)
-	if err != nil {
-		return C.CString(err.Error())
-	}
-	b, _ := json.Marshal(&resp)
-	return C.CString(string(b))
+	return doReq(s, sign.PsbtSignPubKeyHash)
 }
 
 //export PsbtFinalize
 func PsbtFinalize(s *C.char) *C.char {
-	var req, resp sign.Psbt
-	if err := json.Unmarshal([]byte(C.GoString(s)), &req); err != nil {
-		return C.CString(err.Error())
-	}
-
-	p, err := psbt.NewFromRawBytes(strings.NewReader(req.PsbtB64), true)
-	if err != nil {
-		return C.CString(err.Error())
-	}
-
-	if err = psbt.MaybeFinalizeAll(p); err != nil {
-		return C.CString(err.Error())
-	}
-
-	if resp.PsbtB64, err = p.B64Encode(); err != nil {
-		return C.CString(err.Error())
-	}
-
-	b, _ := json.Marshal(&resp)
-	return C.CString(string(b))
+	return doReq(s, func(req *sign.Psbt) (resp sign.Psbt, err error) {
+		p, err := psbt.NewFromRawBytes(strings.NewReader(req.PsbtB64), true)
+		if err != nil {
+			return
+		}
+		if err = psbt.MaybeFinalizeAll(p); err != nil {
+			return
+		}
+		resp.PsbtB64, err = p.B64Encode()
+		return
+	})
 }
 
 //export FreeCString
