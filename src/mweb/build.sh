cfcf0148175241a4ce22ad0bbd85751c5ffb78ce
Hector Chu <hectorchu@gmail.com>
2026-02-21 22:23:09 +0000

    Add mweb arm binary

--- src/mweb/build.sh
+++ src/mweb/build.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-go build -buildmode=c-shared .
+
+OUTPUT=mweb
+
+if [ "$1" = "arm" ]; then
+    export CC=arm-unknown-linux-gnueabi-gcc
+    export CGO_ENABLED=1
+    export GOARCH=arm
+    export GOARM=6
+    export GOOS=linux
+    OUTPUT=mweb-arm
+fi
+
+go build -buildmode=c-shared -ldflags="-s -w" -o $OUTPUT
