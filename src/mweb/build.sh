dac41ac860276ada13c6876810e7d1f38ae2a6a6
Hector Chu <hectorchu@gmail.com>
2026-02-24 10:25:37 +0000

    Need to re-add -O2 flag

--- src/mweb/build.sh
+++ src/mweb/build.sh
@@ -4,7 +4,7 @@ OUTPUT=mweb
 
 if [ "$1" = "arm" ]; then
     export CC=arm-linux-gnueabihf-gcc
-    export CGO_CFLAGS=-mcpu=arm1176jzf-s
+    export CGO_CFLAGS="-mcpu=arm1176jzf-s -O2"
     export CGO_ENABLED=1
     export GOARCH=arm
     export GOARM=6
@@ -12,4 +12,7 @@ if [ "$1" = "arm" ]; then
     OUTPUT=mweb-arm
 fi
 
-go build -buildmode=c-shared -ldflags="-s -w" -o $OUTPUT
+go build -buildmode=c-shared \
+         -buildvcs=false \
+         -ldflags="-s -w" \
+         -o $OUTPUT
