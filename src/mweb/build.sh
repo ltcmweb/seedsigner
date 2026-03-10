dabfaa626ae6125e9c64e3e63f9a0d082974bcd6
Hector Chu <hectorchu@gmail.com>
2026-03-10 22:36:21 +0000

    Simplify mweb build script

--- src/mweb/build.sh
+++ src/mweb/build.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-if [ "$1" = arm ]; then
-    export CC=arm-linux-gnueabihf-gcc
-    export CGO_CFLAGS="-mcpu=arm1176jzf-s -O2"
-    export CGO_ENABLED=1
-    export GOARCH=arm
-    export GOARM=6
-    export GOOS=linux
-fi
+export CC=arm-linux-gnueabihf-gcc
+export CGO_CFLAGS="-mcpu=arm1176jzf-s -O2"
+export CGO_ENABLED=1
+export GOARCH=arm
+export GOARM=6
+export GOOS=linux
 
 go build -buildvcs=false -ldflags="-s -w"
